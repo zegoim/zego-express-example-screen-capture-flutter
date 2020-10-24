129c051fa9cd19027d7e28de4e408f9552151f07
Patrick Fu <patrickfu@zego.im>
2020-10-24 17:02:54 +0800

    build: init

---
+++ src/android/app/src/main/java/im/zego/express/example/screencapture/zego_express_screen_capture/MainActivity.java
@@ -0,0 +1,6 @@
+package im.zego.express.example.screencapture.zego_express_screen_capture;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
