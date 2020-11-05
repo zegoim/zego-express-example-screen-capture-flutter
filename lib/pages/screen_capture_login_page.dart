d20b7ac7247fc8b07cb6a7da7a92143606c3f05b
lizhanpeng <lizhanpeng@zego.im>
2020-11-05 17:44:32 +0800

    fix: bitrate change from 1500 kbps to 3000 kbps

--- lib/pages/screen_capture_login_page.dart
+++ lib/pages/screen_capture_login_page.dart
@@ -68,7 +68,7 @@ class _ScreenCaptureLoginPageState extends State<ScreenCaptureLoginPage> {
 
     // Set necessary params (just for iOS)
     await manager.setParamsForCreateEngine(ZegoConfig.instance.appID, ZegoConfig.instance.appSign, ZegoConfig.instance.isTestEnv, true);
-    await manager.setParamsForVideoConfig(window.physicalSize.width.toInt(), window.physicalSize.height.toInt(), 15, 1500);
+    await manager.setParamsForVideoConfig(window.physicalSize.width.toInt(), window.physicalSize.height.toInt(), 15, 3000);
     await manager.setParamsForStartLive(ZegoConfig.instance.roomID, ZegoConfig.instance.userID, ZegoConfig.instance.userName, ZegoConfig.instance.streamID);
 
     // Start screen capture
